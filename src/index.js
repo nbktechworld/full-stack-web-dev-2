db445731de0b196b4ea0ba1af0fb72cbf76ee466
nbkhope <ryumemaru@yahoo.com>
2024-07-04 19:49:10 -0400

    Day 4 Session 1

---
+++ src/index.js
@@ -0,0 +1,6 @@
+function changeBackground(){
+  const paragraphElement = document.getElementById('first-paragraph')
+  paragraphElement.style.backgroundColor = 'lightblue';
+  paragraphElement.style.color = 'black';
+  paragraphElement.textContent = 'Hello World!';
+}
